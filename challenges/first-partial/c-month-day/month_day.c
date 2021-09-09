b67b669fb44f9f09e3384a2377c077a0fa61e2cc
CArmenta5 <carlosraulap@outlook.com>
2021-09-09 00:12:39 +0000

    Solve first-partial

--- challenges/first-partial/c-month-day/month_day.c
+++ challenges/first-partial/c-month-day/month_day.c
@@ -1,8 +1,84 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+// CAMBIAR LIBRERIAS NO USADAS!!!!!!!!!!!!!!
+// Chear año 
+static char daytab[2][12] = {
+    { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+    };
+
+static char *name[] = {
+       "January", "February", "March",
+       "April", "May", "June",
+       "July", "August", "September",
+       "October", "November", "December"
+    };
+    
+int leap(int year, int yearday){
+    if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+        if(yearday > 366 || yearday == 0){
+            printf("Se mandó un día fuera de los limites del año.\n");
+            return -1;
+        }
+        return 1;
+    }else{
+        if(yearday > 365 || yearday == 0){
+            printf("Se mandó un día fuera de los limites del año.\n");
+            return -1;
+        }
+        return 0;
+    }
+}
 
 /* month_day function's prototype*/
-void month_day(int year, int yearday, int *pmonth, int *pday);
+void month_day(int year, int yearday, int *pmonth, int *pday){
+    //Check if the number is a bisiesto
+    int bisiesto = leap(year, yearday);
+    if(bisiesto != -1){
+        int acum = 0;
+        int res = 0;
+        
+        for(*pmonth = 0; *pmonth < 12; *pmonth = *pmonth +1 ){
+            if(daytab[bisiesto][*pmonth] + acum >= yearday ){
+                *pday = daytab[bisiesto][*pmonth] - ((daytab[bisiesto][*pmonth] + acum) - yearday);
+                break;
+            }else{
+                acum += daytab[bisiesto][*pmonth];
+            }
+        }
 
-int main() {
+        printf("%s %d, %d\n", name[*pmonth], *pday, year);
+    }
+  
+}
+
+int main(int argc, char **argv) {
+    if(argc != 3){
+        printf("No se mandaron los datos correctamente.\n");
+        return -1;
+    }
+    for(int i = 0; i < 2; i++){
+        char *number = argv[1 + i];
+        for(int j = 0; number[j] != 0; j++){
+            if(!isdigit(number[j])){
+                printf("Se han mandado dos parametros que no son enteros.\n");
+                return -1;
+            }
+        }
+    }
+    int year = atoi(argv[1]);
+    int yearday = atoi(argv[2]);
+    int month = 0;
+    int *pmonth = &month;
+    int day = 0;
+    int *pday = &day;
+
+    month_day(year, yearday, pmonth, pday);
+
+    
     return 0;
 }
--- challenges/first-partial/go-functions-methods/geometry.go
+++ challenges/first-partial/go-functions-methods/geometry.go
@@ -7,18 +7,60 @@
 //!+point
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
-type Point struct{ X, Y float64 }
+type Point struct {
+	x, y float64
+}
+
+func (p *Point) X() float64 {
+	return p.x
+}
+func (p *Point) Y() float64 {
+	return p.y
+}
 
 // traditional function
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
+}
+
+func segmentIntersect(p1, p2, p3 Point) float64 {
+	resu := (p2.Y()-p1.Y())*(p3.X()-p2.X()) - (p3.Y()-p2.Y())*(p2.X()-p1.X())
+
+	return resu
 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
+}
+
+func (p *Point) fixPoint() {
+	min := -100
+	max := 100
+	rand.Seed(time.Now().UnixNano())
+	p.x = float64(rand.Intn(max-min+1) + min)
+	rand.Seed(time.Now().UnixNano())
+	p.y = float64(rand.Intn(max-min+1) + min)
+}
+
+func collinearVerif(p1 Point, p2 Point, p3 Point) bool {
+	ab := math.Hypot(p2.X()-p1.X(), p2.Y()-p1.Y())
+	bc := math.Hypot(p3.X()-p2.X(), p3.Y()-p2.Y())
+	ac := math.Hypot(p3.X()-p1.X(), p3.Y()-p1.Y())
+
+	if ab+bc == ac {
+		return true
+	}
+	return false
 }
 
 //!-point
@@ -40,6 +82,120 @@ func (path Path) Distance() float64 {
 }
 
 func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			if len(os.Args) == 1 {
+				fmt.Println("No se ingreso ningun parametro -> ", err)
+			}
+		}
+	}()
+
+	flagDen := false
+	sides, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("Se envió un parametro no permitido.\n")
+	} else if sides < 3 {
+		fmt.Printf("Escribre como parametro un numero mayor o igual a 3.\n")
+		flagDen = true
+	} else if len(os.Args) != 2 {
+		fmt.Printf("Favor de escribir solamente el numero de lados <<sides>>.\n")
+		flagDen = true
+	}
+
+	if err == nil && flagDen == false {
+		min := -100
+		max := 100
+		path := Path{}
+		// Rellena los puntos
+		for i := 0; i < sides; i++ {
+			rand.Seed(time.Now().UnixNano())
+			x := float64(rand.Intn(max-min+1) + min)
+			rand.Seed(time.Now().UnixNano())
+			y := float64(rand.Intn(max-min+1) + min)
+			p := Point{x, y}
+			path = append(path, p)
+		}
+		exFlag := false
+		for i := 0; i < sides || exFlag; i++ {
+			if exFlag{
+				i = 0
+			}
+			first := true
+			last := false
+			exFlag = false
+			for j := i + 1; j < sides; j++ {
+				flag := true
+				if j == sides-1 {
+					last = true
+				}
+				for first || last {
+					if collinearVerif(path[i], path[(i+1)%sides], path[(j+1)%sides]) {
+						path[(j+1)%sides].fixPoint()
+						i = 0
+						exFlag = true
+					} else {
+						first = false
+						last = false
+					}
+				}
+				if i == sides-1 && exFlag == true {
+					break
+				}
+				for flag && j-i > 1 {
+					o1 := segmentIntersect(path[i], path[(i+1)%sides], path[j])
+					if o1 > 0 {
+						o1 = 1
+					} else {
+						o1 = -1
+					}
+					o2 := segmentIntersect(path[i], path[(i+1)%sides], path[(j+1)%sides])
+					if o2 > 0 {
+						o2 = 1
+					} else {
+						o2 = -1
+					}
+					o3 := segmentIntersect(path[j], path[(j+1)%sides], path[i])
+					if o3 > 0 {
+						o3 = 1
+					} else {
+						o3 = -1
+					}
+					o4 := segmentIntersect(path[j], path[(j+1)%sides], path[(i+1)%sides])
+					if o4 > 0 {
+						o4 = 1
+					} else {
+						o4 = -1
+					}
+					if o1 != o2 && o3 != o4 {
+						if j == sides-1 {
+							path[(j+1)%sides].fixPoint()
+						} else {
+							path[(j+1)%sides].fixPoint()
+						}
+						i = 0
+						exFlag = true
+					} else {
+						flag = false
+					}
+				}
+			}
+		}
+		fmt.Printf("Generating a [%d] sides figure\n", sides)
+		fmt.Printf("Figure's vertices\n")
+		for i := 0; i < sides; i++ {
+			fmt.Printf("(  %g,  %g)\n", path[i].x, path[i].y)
+		}
+		fmt.Printf("Figure's Perimeter\n")
+		for i := 0; i < sides; i++ {
+			if i < sides-1 {
+				fmt.Printf("%.2f  + ", math.Floor(path[i].Distance(path[(i+1)%sides])*100)/100)
+			} else {
+				fmt.Printf("%.2f = ", math.Floor(path[sides-1].Distance(path[0])*100)/100)
+			}
+		}
+		total := math.Floor(path.Distance()*100)/100 + math.Floor(path[sides-1].Distance(path[0])*100)/100
+		fmt.Printf("%.2f\n", total)
+	}
 }
 
 //!-path
