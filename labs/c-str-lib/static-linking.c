147c3d8522f1e4000a43855e719a5abfc9d87187
CArmenta5 <carlosraulap@outlook.com>
2021-10-01 01:38:44 +0000

    Solved c-str-lib

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+/*Funciones de ayuda*/
+int strCompare(char *origin, char *check){
+    int i = 0;
+    if(mystrlen(origin) != mystrlen(check)){
+        return -1;
+    }
+    for(; origin[i] != '\0'; i++){
+        if (origin[i] != check[i]){
+            return -1;
+        }
+    }
+    return 0;
+
+}
+
+int main(int argc, char **argv) {
+
+    char *operacion;
+    if(argc != 4){
+        printf("No se han mandado la cantidad de parametros necesarios.\n");
+        printf("-add/-find \"<<cadena>>\" \"<<cadena>>\".\n");
+        return 0;
+    }
+    if(strCompare(argv[1],"-add") == 0 || strCompare(argv[1],"-find") == 0){
+        operacion = argv[1];
+    }else{
+        printf("No se mando la operacion correcta.\n");
+        printf("-add/-find \"<<cadena>>\" \"<<cadena>>\".\n");
+        return 0;
+    }
+    if (strCompare(operacion,"-add") == 0){
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        char* new = mystradd(argv[2], argv[3]);
+        printf("New String          : %s\n", new);
+        printf("New length          : %d\n", mystrlen(new));
+    }else{
+        printf("['%s'] string was found at [%d] position\n", argv[3], mystrfind(argv[2], argv[3]));
+    }
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,49 @@
-
+#include <stdlib.h>
 
 int mystrlen(char *str){
-    return 0;
+    int length = 0;
+    for(int i = 0; str[i] != '\0' ;i++){
+        length++;
+    }
+    return length;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int lenOrigin = mystrlen(origin);
+    int lenAddition = mystrlen(addition);
+
+    // Create a new var to avoid override the params
+    char *newStr = (char *)malloc(mystrlen(origin) + 1);
+   
+    int i = 0;
+    for(; i < lenOrigin; i++){
+        newStr[i] = origin[i];
+    }
+    for(i = 0; i < lenAddition; i++){
+        newStr[lenOrigin + i] = addition[i];
+    }
+
+    newStr[lenOrigin + i] = '\0';
+
+    return newStr;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int j, i, prev;
+    for(i = 0; origin[i] != '\0'; i++){
+        prev = i;
+        for(j = 0; substr[j] != '\0'; j++){
+            if (origin[i] == substr[j] && origin[i] != '\0' && substr[j] != '\0'){
+                i++;
+            }else{
+                i = prev + j;
+                break;
+            }
+        }
+        if(substr[j] == '\0'){
+            return prev;
+        }
+    }
+    return -1;
 }
+
